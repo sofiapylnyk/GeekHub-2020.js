51bca492563ec82b276e44aeded8c4469c93c055
Sofia Pylnyk <46477318+sofiapylnyk@users.noreply.github.com>
2020-10-26 10:20:58 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -31,7 +31,7 @@ document.querySelectorAll('input[name], #formula').forEach(function (input) {
 				throw "returned value isn't logical";
 			}
 		} catch (error) {
-			result.style.backgroundColor = "White";
+			result.style.backgroundColor = "#fff";
 			console.error(error);
 		}
 	});
