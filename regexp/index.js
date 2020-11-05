bd5645fd1aeba67c0055659b8b46383bc578e1a6
Sofia Pylnyk <46477318+sofiapylnyk@users.noreply.github.com>
2020-11-05 20:06:18 +0200

    Update index.js

--- regexp/index.js
+++ regexp/index.js
@@ -2,38 +2,39 @@ document.querySelector('#user-form').addEventListener('submit', function (e) {
 
  e.preventDefault();
 
- let fulName = document.querySelector('[name="full_name"]');
+ let fullName = document.querySelector('[name="full_name"]');
  let email = document.querySelector('[name="email"]');
  let password = document.querySelector('[name="password"]');
  let description = document.querySelector('[name="description"]');
 
- let validFulName = /^[а-щьюяїієґ]+\s+[а-щьюяїієґ]+\s+[а-щьюяїієґ]+$/i;
- let validEmail = /^(\w\.?)+[^.\s]@\w+\.\w+\s*$/;
+ let validFullName = /^[а-щьюяїієґ]+\s+[а-щьюяїієґ]+\s+[а-щьюяїієґ]+$/i;
+ let validEmail = /^[a-z0-9-]+[a-z0-9-\.]+[a-z0-9-]@[a-z0-9-][a-z0-9-.]+[a-z0-9-]+$/;
  let validPassword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\S+$).{8,}$/;
 
-
-     if(validFulName.test(fulName.value)){
-     fulName.style.backgroundColor = '#C2E0C6';
+     if (validFullName.test(fullName.value.trim())){
+     	fullName.style.backgroundColor = '#C2E0C6';
      } else{
-     fulName.style.backgroundColor = '#F9D0C4';
+     fullName.style.backgroundColor = '#F9D0C4';
      }
-		 if(validEmail.test(email.value.trim())){
-			 email.style.backgroundColor = '#C2E0C6';
-		 } else{
-			 email.style.backgroundColor = '#F9D0C4';
-		 }
-		 if(validPassword.test(password.value)){
-			 password.style.backgroundColor = '#C2E0C6';
-		 } else{
-			 password.style.backgroundColor = '#F9D0C4';
-		 }
+
+     if (validEmail.test(email.value.trim())){
+   	email.style.backgroundColor = '#C2E0C6';
+     } else{
+	email.style.backgroundColor = '#F9D0C4';
+     }
+
+     if (validPassword.test(password.value)){
+	password.style.backgroundColor = '#C2E0C6';
+     } else{
+	password.style.backgroundColor = '#F9D0C4';
+     }
 });
 
 	 document.querySelectorAll('[data-show]').forEach(function (button){
-					 button.addEventListener('click', function (e) {
-									 document.querySelector('#description').classList.add('d-none');
-									 document.querySelector('#preview').classList.add('d-none');
+		button.addEventListener('click', function (e) {
+		document.querySelector('#description').classList.add('d-none');
+		document.querySelector('#preview').classList.add('d-none');
 
-									 document.querySelector('#' + e.currentTarget.getAttribute('data-show')).classList.remove('d-none');
+		document.querySelector('#' + e.currentTarget.getAttribute('data-show')).classList.remove('d-none');
  });
 });
