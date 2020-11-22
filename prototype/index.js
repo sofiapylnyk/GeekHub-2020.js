3915c228685a268db349de2a70447ff78d7e22bd
Sofia Pylnyk <46477318+sofiapylnyk@users.noreply.github.com>
2020-11-22 17:19:05 +0200

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -2,40 +2,41 @@ function Csv() {
 };
   Csv.prototype.parse = function parse(string, separator="") {
     if (separator === "") {
-	  let rows = string.split("\n");
+	  const rows = string.split("\n");
 	  const seps = [',', ';', '\t'];
+	    
 	  let sepIdx = 0;
 	  let row = rows[0];
 	  let position;
-	  let count1 = count2 = 0;
+	  let count1 = 0;
+	  let count2 = 0;
 	  let found = false;
 
-	  for (; !found; sepIdx++) {
+	  for (; !found; sepIdx+=1) {
 		separator = seps[sepIdx];
 		position = row.search(separator);
 
 		if (position >= 0) {
-	  	  count1 = count2 = 0;
+	  	  count1 = 0;
+		  count2 = 0;
 		  row = rows[0];
 
 		  while (position >= 0) {
 		    row = row.slice(position + 1);
-			position = row.search(separator);
-			count1 += 1;
+		    position = row.search(separator);
+		    count1 += 1;
 		  }
 		  row = rows[1];
 		  position = row.search(separator);
 
 		  while (position >= 0) {
 		    row = row.slice(position + 1);
-			position = row.search(separator);
-			count2 += 1;
+		    position = row.search(separator);
+		    count2 += 1;
 		  }
 		  found = count1 == count2;
 		}
-
 	  }
-
 	}
 		return string.split('\n').map(x => x.split(separator));
   };
@@ -44,8 +45,8 @@ function Csv() {
 	const lastCol = array[0].length - 1;
 	let string = "";
 
-	for (let i = 0; i < rows; i++) {
-	  for (let j = 0; j < lastCol; j++) {
+	for (let i = 0; i < rows; i+=1) {
+	  for (let j = 0; j < lastCol; j+=1) {
 		string += array[i][j] + separator;
 	  }
 	  string += array[i][lastCol] + "\n";
